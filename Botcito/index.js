b0ddf1ab7a085d6b76e3e2f113f0da8c3dc77eae
Ziroislazy <zoeramos2008@gmail.com>
2026-09-20 23:24:59 -0300

    Configure gitignore protection and remove stray files

---
+++ Botcito/index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/el-botcito-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `What's up/Que onda papu\nLatency: ${latency}ms` });
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
+
+app.command("/el-botcito-help", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Available Commands:
+/el-botcito-ping - Check bot latency
+/el-botcito-catfact - Get a cat fact`
+  });
+});
